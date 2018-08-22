466d08460ba2048af46b3502b161b3257c1429a1
Hridya Valsaraju <hridya@google.com>
2018-08-21 21:22:37 -0700

    Build bootctrl.sdm845 using Soong
    This is required to build the recovery variant of the
    same to be used by fastbootd.
    
    Bug: 78793464
    Test: make
    Change-Id: I16eddcb853bda1a9969810f258884ebdbbfc1f5c
    
    lel
    
    Change-Id: I5e1d2d9761f6a2d631bbfae289cd144375d26b0f
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -68,6 +68,7 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES_DEBUG += \
+    android.hardware.boot@1.0-impl.recovery \
     bootctl
 
 # Common init scripts
@@ -177,16 +178,11 @@ PRODUCT_BOOT_JARS += \
 
 # Update engine
 PRODUCT_PACKAGES += \
+    bootctrl.sdm845.recovery \
     brillo_update_payload \
     update_engine \
     update_engine_sideload \
     update_verifier
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.sdm845 \
-    libcutils \
-    libgptutils.oneplus_sdm845.recovery \
-    libz \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
