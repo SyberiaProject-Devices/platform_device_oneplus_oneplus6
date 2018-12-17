a6f72347f6b467ed40e88e0ccc485fc712d82307
millerliang <millerliang@google.com>
2018-12-17 15:47:24 +0800

    system_prop: Refined early SF phase offsets
    Sometimes SW_VSNC of DispSync comes 1ms earlier than signal_retire_fence(or HW_VSYNC)
    It means VSYNC-sf only 5ms behind signal_retire_fence.
    And VSYNC-sf will be 5.5ms earlier when applying early_sf_phase.
    (the phase-sf changes from 6ms to 0.5ms)
    
    Therefore, VSYNC-sf comes 0.5ms earlier than signal_retire_fence when
    applying early_sf_phase for transaction and cause frame missed.
    
    Add more 1ms to early_sf_phase then it will come 0.5 later than signal_retire_fence
    in this case.
    
    Bug: 118606025
    Test: Open/close apps, observe no jank
    Test: -Launch app, stay the same picture on screen at least 30s
          -Tap home key, observe no jank when doing app animation
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Iec83aef719b830aed612b47bbda910508406d196

--- system_prop.mk
+++ system_prop.mk
@@ -52,14 +52,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.gfx_ubwc_disable=0 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.enable_gl_backpressure=1 \
-    debug.sf.early_phase_offset_ns=500000 \
-    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_phase_offset_ns=1500000 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
     persist.vendor.color.matrix=2 \
     debug.cpurend.vsync=false
+
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cne.feature=1
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpm.feature=9
+
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1  \
+    persist.dbg.wfc_avail_ovr=1
 
 # OTG
 PRODUCT_PROPERTY_OVERRIDES += \
