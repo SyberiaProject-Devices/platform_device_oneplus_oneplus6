aexmod-tmp/platform_device_oneplus_enchilada

set client package name
* Introduce a new boot receiver that force camera client package name via HIDL HAL to fix portrait mode

Change-Id: Ibac00612b794eefd3c6d7ba6fa4e44fcc0197cc1
Signed-off-by: DennySPB <dennyspb@gmail.com>

CameraPackage/Android.mk
@@ -1,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),enchilada))
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := CameraPackage
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_USE_AAPT2 := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_DEX_PREOPT := false
+
+include $(BUILD_PACKAGE)
+
+endif

SyberiaProject-Devices/platform_device_oneplus_oneplus6

CameraPackage/Android.mk
@@ -1,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),oneplus6))
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := CameraPackage
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_USE_AAPT2 := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_DEX_PREOPT := false
+
+include $(BUILD_PACKAGE)
+
+endif