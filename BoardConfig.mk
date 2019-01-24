7bc0cbf228fda65a702e7b1e65bc5d14fcc37ebd
celtare21 <celtare21@gmail.com>
2019-01-24 11:21:07 +0000

    BoardConfig: Properly set dexpreopt
    Change-Id: Icb2e129000676938b75b94c1279b8df676a4ed21
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,13 +105,17 @@ TARGET_USES_MEDIA_EXTENSIONS := false
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# Dex
+# Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_DEBUG_INFO := false
+      USE_DEX2OAT_DEBUG := false
+      DONT_DEXPREOPT_PREBUILTS := true
+      WITH_DEXPREOPT_PIC := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 TARGET_USES_HWC2 := true
