36ef0031c0a188969650b793a7ea71126c9410ad
Park Ju Hyung <qkrwngud825@gmail.com>
2019-02-18 13:57:31 +0300

    replace msm_irqbalance.conf
    Use a better selection of interrupts list
    
    @DennySPB: cherry-picked from
    https://github.com/arter97/android_kernel_oneplus_sdm845/commit/decbed66214d6f071ba957ce7f241b53dfc5b414
    https://github.com/arter97/android_kernel_oneplus_sdm845/commit/57e229e706f1e80ea8bc4b9926c2d8a4442d0963
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -62,7 +62,9 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/etc/msm_irqbalance.conf
+
 
 # Boot control
 PRODUCT_PACKAGES_DEBUG += \
