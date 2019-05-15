a306bae8e45081c05d78a4d4dcf45403ed92bfe4
DennySPB <dennyspb@gmail.com>
2019-05-15 11:23:47 +0300

    Build as much as possible from sources
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I84c80ec53b2e1a53a5db7273fc06cee8c2dbd16b
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Id33094dead61b2eca586f2b29696107bef6f11ab

--- device.mk
+++ device.mk
@@ -84,7 +84,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Display
 PRODUCT_PACKAGES += \
     libvulkan \
+    libdisplayconfig \
+    libqdMetaData.system \
     vendor.display.config@1.7
+
 
 # Hostapd
 PRODUCT_COPY_FILES += \
@@ -136,6 +139,9 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1 \
+    android.hardware.secure_element@1.0 \
+    vendor.nxp.nxpese@1.0 \
+    vendor.nxp.nxpnfc@1.0 \
     com.android.nfc_extras \
     Tag
 
