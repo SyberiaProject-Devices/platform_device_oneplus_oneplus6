c6432addaf8353a80f3636fbb39954bb50f37027
DennySPB <dennyspb@gmail.com>
2019-05-28 12:04:22 +0300

    gpt-utils: use generated kernel headers
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Ife42f18b060e140a6adb6277926f223e5c6a6de3
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- gpt-utils/Android.mk
+++ gpt-utils/Android.mk
@@ -1,20 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := bootable/recovery \
-		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE := libgptutils
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := bootable/recovery \
-		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES := bootable/recovery
 LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_SHARED_LIBRARIES += liblog libcutils
 LOCAL_MODULE := libgptutils
 LOCAL_COPY_HEADERS_TO := gpt-utils/inc
