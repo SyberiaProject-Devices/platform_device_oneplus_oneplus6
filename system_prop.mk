9b83b01cda1d3cdac353b70b047b367259bcb24f
LuK1337 <priv.luk@gmail.com>
2019-06-15 15:40:37 +0200

    Update ssr restart level prop
    Change-Id: Icaca77128671f1b0180cc3315f062f3f123f8b04
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -117,7 +117,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.ssr.restart_level=ALL_ENABLE
+    persist.vendor.ssr.restart_level=ALL_ENABLE
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
