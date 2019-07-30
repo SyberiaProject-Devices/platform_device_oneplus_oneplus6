cdbdf03976f27dd07ecc9484d0c6b08e261743a3
Bruno Martins <bgcngm@gmail.com>
2019-07-30 14:21:52 +0100

    Compile OMX from source
    Change-Id: I0e55d49e96423a9473059cf9000b0114b903d277
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -210,6 +210,18 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/lib64/pixel-power-ext-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib64/pixel-power-ext-V1-ndk_platform.so
 
 # Media
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
