f0581e56709cb00aaa6fd102c82269197cfc49d8
LuK1337 <priv.luk@gmail.com>
2019-09-19 10:05:51 +0300

    Update blobs from DP3 preview image
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I64ee38002590e783ca25296072d277da7d3541b4
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -134,6 +134,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0:64 \
     android.hardware.nfc@1.1:64 \
+    android.hardware.nfc@1.2:64 \
     android.hardware.secure_element@1.0:64 \
     com.android.nfc_extras \
     Tag \
