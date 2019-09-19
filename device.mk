TeamWin/android_device_google_crosshatch

Build libgptutils with Android.bp
This is required to build bootctrl.sdm845 with Android.bp.

Bug: 78793464
Test: make

Change-Id: Iff4f50ad363cc003d60a5380addd79749b547514

device.mk
@@ -168,7 +168,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # boot_control HAL and its dependencies.
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.sdm845 \
-    libgptutils \
+    libgptutils.crosshatch.recovery \
     libz \
     libcutils
 

SyberiaProject-Devices/platform_device_oneplus_oneplus6

Signed-off-by: DennySPB <dennyspb@gmail.com>

device.mk
@@ -185,7 +185,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.sdm845 \
     libcutils \
-    libgptutils \
+    libgptutils.oneplus_sdm845.recovery \
     libz \
 
 PRODUCT_PACKAGES_DEBUG += \