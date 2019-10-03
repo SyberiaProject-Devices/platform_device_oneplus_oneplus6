b88132431cca1e0c9a775d2e06dec70115ed8a81
Santhosh Behara <santhoshbehara@codeaurora.org>
2019-10-03 18:26:59 +0530

    media: Add and publish Codec2 HAL service
    *Add 2 codec2 relates libs to PRODUCT_PACKAGES in order to copy
    them to /vendor/lib/
    *Add seccomp policies for codec2 services
    
    Change-Id: I83af4f6b80886ca18aa059285e0b94403c027e29
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -114,6 +114,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+# Codec2
+PRODUCT_PACKAGES += \
+    libcodec2_vndk.vendor \
+    libcodec2_hidl@1.0.vendor
+
 # Context Hub
 PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-impl.generic \
@@ -236,6 +241,7 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 PRODUCT_PACKAGES += \
