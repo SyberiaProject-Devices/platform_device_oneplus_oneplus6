b80eaf34b9e9c08049afc7c7daf419183d748224
1nfrag <sil3ntcll@gmail.com>
2019-11-07 13:05:08 +0800

    Build AOSP BT 2.0 audio hal
    Change-Id: I3c7120f5f29605a939d0ca78054894e2cafcca65
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -110,6 +110,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
     libbthost_if \
     libldacBT_dec \
     BluetoothQti \
