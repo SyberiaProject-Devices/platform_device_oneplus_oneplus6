b986aece44b1fb5ab355f737495f8f2d2ccc171a
DennySPB <dennyspb@gmail.com>
2019-11-08 15:22:34 +0300

    BoardConfig: remove ART flags
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I02c98f48580424493145f7a6582a8a75bfb70f1d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,18 +117,6 @@ TARGET_USES_MEDIA_EXTENSIONS := false
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_DEBUG_INFO := false
-      USE_DEX2OAT_DEBUG := false
-      DONT_DEXPREOPT_PREBUILTS := true
-      WITH_DEXPREOPT_PIC := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-  endif
-endif
-
 # Display
 TARGET_USES_HWC2 := true
 
