e24ddb52e903cff6f67b7b2495a3180194797a37
DennySPB <dennyspb@gmail.com>
2019-11-08 09:33:21 +0300

    add back power hal
    This reverts commit 69143d0f7bfc8c5db66601f2942382b82f60edfc.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I1905db599cb1a6ecedbb7d844b1ba470b3f3b9ec

---
+++ power/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := power.qcom
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib64
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+    hint-data.c \
+    list.c \
+    metadata-parser.c \
+    power-845.c \
+    power.c \
+    powerhintparser.c \
+    utils.c
+
+LOCAL_C_INCLUDES := external/libxml2/include \
+                    external/icu/icu4c/source/common
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
+
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+
+LOCAL_CFLAGS += -Werror -Wall -Wno-unused-parameter
+LOCAL_CFLAGS += -DINTERACTION_BOOST
+
+include $(BUILD_SHARED_LIBRARY)
