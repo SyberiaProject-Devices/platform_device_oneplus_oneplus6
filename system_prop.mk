57a1b62aba60f9ee41705932bdee6ff62936a205
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-21 15:32:18 +0100

    sdm845-common: Enable value added AOSP features
    Change-Id: Id988dabcb97d38313d0e29b7aa87ec68418a9217
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -127,3 +127,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
     debug.sf.enable_hwc_vds=1
+
+# Vendor
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
