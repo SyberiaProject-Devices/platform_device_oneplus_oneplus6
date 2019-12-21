f1a20783f2c12d5521f9568e98e304558ee2873b
TheStrix <parthbhatia98@gmail.com>
2019-12-21 15:04:01 +0530

    oneplus6: Update blobs from OxygenOS stable 10.3.0
     - Also add/build missing dependent blobs.
     - Arrange blob list.
    
    Change-Id: I549a826dadfcddcefafee7d03b2c3b4ebd556b51
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -75,7 +75,23 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default
+    audio.a2dp.default \
+    libaudio-resampler \
+    libstagefright_softomx
+
+# Media
+PRODUCT_PACKAGES += \
+    libmediaplayerservice
+
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.deprecated@1.0
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
