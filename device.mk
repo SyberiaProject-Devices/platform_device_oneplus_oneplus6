d15b1375f72076133e90d48029fc8be86eee8b37
DennySPB <dennyspb@gmail.com>
2019-12-26 13:23:57 +0300

    Revert "use stock audio policy"
    This reverts commit fc49080897b366ecbd1303abd7f1120ed407ea57.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I07f44070ae0a8a997bb0870130274928c6712da8
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -82,6 +82,8 @@ PRODUCT_PACKAGES += \
     RemovePackages
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/android.hardware.graphics.composer@2.3-service.rc:system/product/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/android.hardware.graphics.composer@2.3-service.rc \
     $(LOCAL_PATH)/configs/powerhint.json:system/etc/powerhint.json \
