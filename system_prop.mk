132cae23daf254945dfa81406b4b4084f00458c6
DennySPB <dennyspb@gmail.com>
2019-12-26 13:18:16 +0300

    Revert "prop: set surface flinger max accure buffers to 3"
    This reverts commit 08b16882e65590416cd11294697e31860106907a.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I0fdf95e3b5d134b1eb1447064a66310413433706

--- system_prop.mk
+++ system_prop.mk
@@ -58,7 +58,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     persist.vendor.color.matrix=2 \
     debug.cpurend.vsync=false
 
