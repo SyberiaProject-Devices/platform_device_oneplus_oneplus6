0d515009229da8067c21e31689322bfb3739670c
Jake Weinstein <jake@aospa.co>
2020-01-13 04:11:32 +0100

    oneplus6: switch to new GCC (9.x) for kernel
    Change-Id: I805d0df82f79e821ea7355a9850e27e1a1036b9d
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,10 +62,7 @@ NEED_KERNEL_MODULE_SYSTEM := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CONFIG := syberia_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-9.1/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
-# TARGET_KERNEL_CLANG_COMPILE := true
-# TARGET_KERNEL_CLANG_VERSION := 9.0.3
+TARGET_KERNEL_NEW_GCC_COMPILE := true
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
