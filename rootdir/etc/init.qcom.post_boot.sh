544fa14680559088cf9de073ade9e1eaf6275b14
DennySPB <dennyspb@gmail.com>
2020-01-24 11:01:37 +0300

    init.postboot.sh: cleanup scheduler and schedutil setup
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I8d08b71ef372e1917f10b4af8ef41ad1870f9cf3
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -4078,32 +4078,12 @@ case "$target" in
         echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster
         echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 
-        # Setting b.L scheduler parameters
-        echo 95 > /proc/sys/kernel/sched_upmigrate
-        echo 85 > /proc/sys/kernel/sched_downmigrate
-        echo 100 > /proc/sys/kernel/sched_group_upmigrate
-        echo 95 > /proc/sys/kernel/sched_group_downmigrate
-        echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-
         # configure governor settings for little cluster
         echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/rate_limit_us
-        echo 1228800 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
-        echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/pl
         echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
         # configure governor settings for big cluster
         echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-        echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rate_limit_us
-        echo 1536000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
-        echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/pl
-        echo "0:1324800" > /sys/module/cpu_boost/parameters/input_boost_freq
-        echo 120 > /sys/module/cpu_boost/parameters/input_boost_ms
-
-        #ifdef VENDOR_EDIT
-        # Enable Adaptive LMK liuyaxin@framework.perf change
-        echo "18432,23040,27648,51256,150296,200640" > /sys/module/lowmemorykiller/parameters/minfree
-        #endif VENDOR_EDIT
 
         # Limit the min frequency to 825MHz
         echo 825000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
