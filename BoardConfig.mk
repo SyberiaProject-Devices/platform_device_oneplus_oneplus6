12c66c1218cc2940e178c48ed1e978062ed5897c
Jake Weinstein <jake@aospa.co>
2020-03-04 18:31:59 +0100

    enchilada: remove deprecated TARGET_USES_64_BIT_BINDER
    Change-Id: I7711fd82e0eb0a7d93fd57d25ca38e29a23113a7
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,8 +40,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a75
 
-TARGET_USES_64_BIT_BINDER := true
-
 # Flag for kernel headers generator
 BOARD_USES_QCOM_HARDWARE := true
 
