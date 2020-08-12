a63f8972e6f0fb221f5dd09e8f0db04463b0f95a
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    raphael: create dummy libqti-perfd-client
        * CameraHAL complains about missing perfd and then crashes, so let's
        * create a dummy libqti-perfd-client to shut up the hal
        * we could even map the functions to use libperfmgr powerhints in future
    
        08-12 10:21:24.212 E/CHIUSECASE(22011): [ERROR  ] chxutils.cpp:628 LibMap() Failed to load library libqti-perfd-client.so error dlopen failed: library libqti-perfd-client.so not found
        08-12 10:21:24.212 E/CHIUSECASE(22011): [ERROR  ] chxextensionmodule.cpp:181 ExtensionModule() Failed to load perf lib
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I3e1290307c1d20de2a738c02628a74a1e1732e32

--- device.mk
+++ device.mk
@@ -209,6 +209,7 @@ PRODUCT_PACKAGES += \
 # OnePlus Camera HIDL
 PRODUCT_PACKAGES += \
     libcvface_api \
+    libqti-perfd-client \
     vendor.oneplus.camera.CameraHIDL@1.0 \
     vendor.oneplus.camera.CameraHIDL@1.0-adapter-helper \
     vendor.oneplus.camera.CameraHIDL-V1.0-java
