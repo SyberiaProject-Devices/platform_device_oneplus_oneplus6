e743950a620132ae397056c03f7f7c6983a63480
DennySPb <dennyspb@gmail.com>
2020-09-15 09:04:33 +0300

    [bringup] go permissive and remove broken rules for now
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I7c5707fdca9843b72bca6ab77685a240b92ecbaf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00008000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -151,11 +152,11 @@ TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := $(VENDOR_PATH)/ramdisk-recovery.cpio
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/private \
     device/qcom/sepolicy/qva/private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/public \
     device/qcom/sepolicy/qva/public
 
