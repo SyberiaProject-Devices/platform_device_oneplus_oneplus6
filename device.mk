2a116933172f15cb13085aa3787ccc2ff128e0c7
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    Drop android.hardware.thermal HAL
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -440,11 +440,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# Thermal HAL
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
