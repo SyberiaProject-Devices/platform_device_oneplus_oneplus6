243f2d2b450a68e3e8f3566a7a75ad6430e5491c
DennySPb <dennyspb@gmail.com>
2020-10-07 13:21:03 +0300

    Revert "Switch to AOSP bt stack"
    This reverts commit 30559ed6031d18e84484efa335ba73d72fae7b33.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I90cc2f4a0239ee0a54d689fa0f4d4b56fc262c12

--- device.mk
+++ device.mk
@@ -91,6 +91,13 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libstagefright_softomx
 
+# QTI Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    BluetoothQti
+
 # Media
 PRODUCT_PACKAGES += \
     libmediaplayerservice
