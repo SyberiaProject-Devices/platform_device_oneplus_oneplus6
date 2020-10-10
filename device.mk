7bb9352702236f7b24803cc3712e449bbbb035e7
Alessandro Astone <ales.astone@gmail.com>
2020-10-10 18:50:56 +0200

    Import codec2 seccomp policy
    Change-Id: I9efcb2f71a3917c4e317fdde754fc10905b91cc9
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -268,7 +268,8 @@ PRODUCT_COPY_FILES += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/codec2.vendor.base.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.base.policy
 
 PRODUCT_PACKAGES += \
     libmediaplayerservice
