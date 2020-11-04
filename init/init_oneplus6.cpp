8d45335f274b17181a8af12c4442d60bc776d587
DennySPb <dennyspb@gmail.com>
2020-11-04 15:55:42 +0300

    init: update sunfish's fingerprint
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I663ee45f4fc4f7f5161197ae5509fc40cdae32d1

--- init/init_oneplus6.cpp
+++ init/init_oneplus6.cpp
@@ -106,8 +106,8 @@ void vendor_load_properties()
 {
 	property_override("vendor.audio.feature.spkr_prot.enable", "false");
 	property_override("ro.control_privapp_permissions", "log");
-	property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201005.006/6828489:user/release-keys");
-	property_override_dual("ro.build.fingerprint", "ro.product.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201005.006/6828489:user/release-keys");
+	property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys");
+	property_override_dual("ro.build.fingerprint", "ro.product.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys");
 
 	// Workaround SafetyNet
 	workaround_snet_properties();
