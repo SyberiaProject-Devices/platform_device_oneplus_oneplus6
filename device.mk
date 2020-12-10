a01a93304ec16b0f786e4e9913707237c4ed0ecb
DennySPb <dennyspb@gmail.com>
2020-12-10 11:50:54 +0300

    Build additional QTI bluetooth vendor packages
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: If258640340a1d33c86fbb9f42eb095c8c4102d9a
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -112,10 +112,13 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
+    BluetoothQti \
     libbthost_if \
     libldacBT_dec \
-    BluetoothQti \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
