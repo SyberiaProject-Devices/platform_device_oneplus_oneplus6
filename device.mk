8981824443a59d4da2de826e37ff75fbc61f2012
DennySPb <dennyspb@gmail.com>
2020-12-14 15:58:04 +0300

    Build libtinycompress
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I29bda0bf2dbdce2ef6c2e26d6034fc99396449ae

--- device.mk
+++ device.mk
@@ -97,6 +97,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
+    libtinycompress \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
