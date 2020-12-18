792b90e16a612086838c3795bc34077ffb5f05e6
DennySPb <dennyspb@gmail.com>
2020-12-18 10:04:07 +0300

    Switch to Google's Codec2 HAL from crosshatch
    Kanged davinci's HAL have probe for HEIC codec and crashes
    even when this codec doesn't advertised in media config.
    4.9 kernel have another HEIC capabilities than HAL expect to set.
    Crosshatch C2 HAL haven't HEIC references and works well
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Idd6009ca202e09a5ecbf68b1c8c029a8211cc6b7

--- device.mk
+++ device.mk
@@ -251,6 +251,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
+    libavservices_minijail_vendor \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor \
     libc2dcolorconvert \
