2ccc88a95e5fb6ec4d164d6e5653a54503199939
DennySPb <dennyspb@gmail.com>
2021-01-28 16:17:20 +0300

    Revert "power-libperfmgr: enable DOUBLE_TAP_TO_WAKE mode explicitly"
    This reverts commit ae052a44197ee73c7cf3c9b2dba0ccb77ae021c2.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- power-libperfmgr/aidl/Power.cpp
+++ power-libperfmgr/aidl/Power.cpp
@@ -150,8 +150,8 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
 
 ndk::ScopedAStatus Power::isModeSupported(Mode type, bool *_aidl_return) {
     bool supported = mHintManager->IsHintSupported(toString(type));
-    // LOW_POWER and DOUBLE_TAP_TO_WAKE handled insides PowerHAL specifically
-    if (type == Mode::LOW_POWER || type == Mode::DOUBLE_TAP_TO_WAKE) {
+    // LOW_POWER handled insides PowerHAL specifically
+    if (type == Mode::LOW_POWER) {
         supported = true;
     }
     LOG(INFO) << "Power mode " << toString(type) << " isModeSupported: " << supported;
