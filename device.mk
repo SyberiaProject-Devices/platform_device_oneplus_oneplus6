5298158e8250f4a98e50295d46f2dfb682605b00
Sherif Rahim <sherifrahim2001@gmail.com>
2021-04-12 22:39:26 +0530

    Build MotoDolby v3
    *Port from sofiap_sprout(GPlus) by @reiryuki, (thanks for the helps too..)
    
    files refered here: reiryuki/Moto-Dolby-G-Pro-Audio-FX-Magisk-Module
    reffered commit: HSgod/device_xiaomi_raphael@7708617#diff-7ee7359ba09f5727ba183953197099cff2eacc555c82091a7e3268c87e8e1029
    Signed-off-by: Sherif Rahim <sherifrahim2001@gmail.com>
    Co-authored-by: SonalSingh18 <sonal.singh.19993@gmail.com>
    Change-Id: Ieaac6a32838d7bf086f8ff138eaf1260c7ea811d
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -279,6 +279,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
+    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
 PRODUCT_COPY_FILES += \
