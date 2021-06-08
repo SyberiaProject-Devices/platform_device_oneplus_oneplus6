b5dd5ad4fcd64bd0791d07637681f2cc9413aa13
DennySPb <dennyspb@gmail.com>
2021-06-08 11:46:43 +0300

    Update sunfish fp
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ieb974c82a5a977e91d90742caef74a1942f76e7a

--- init/init_oneplus6.cpp
+++ init/init_oneplus6.cpp
@@ -106,8 +106,8 @@ void vendor_load_properties()
 {
 	property_override("vendor.audio.feature.spkr_prot.enable", "false");
 	property_override("ro.control_privapp_permissions", "log");
-	property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RQ2A.210505.002/7246365:user/release-keys");
-	property_override_dual("ro.build.fingerprint", "ro.product.build.fingerprint", "google/sunfish/sunfish:11/RQ2A.210505.002/7246365:user/release-keys");
+	property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys");
+	property_override_dual("ro.build.fingerprint", "ro.product.build.fingerprint", "google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys");
 
 	// Workaround SafetyNet
 	workaround_snet_properties();
