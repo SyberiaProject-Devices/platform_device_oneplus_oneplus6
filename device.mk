65ece03c88c837cfd7b51ffdd12d32dc80aefd91
DennySPb <dennyspb@gmail.com>
2021-07-19 12:05:57 +0300

    Build android secure element 1.1
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ia2c58c1e025306c59f0b23a7111d8d6ac44cb55a
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -332,6 +332,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1:64 \
     android.hardware.nfc@1.2:64 \
     android.hardware.secure_element@1.0:64 \
+    android.hardware.secure_element@1.1:64 \
     com.android.nfc_extras \
     vendor.nxp.nxpese@1.0:64 \
     vendor.nxp.nxpnfc@1.0:64 \
