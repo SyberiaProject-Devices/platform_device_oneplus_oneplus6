53ec7dd37c34d6c32a8281572bc93875a0674641
DennySPb <dennyspb@gmail.com>
2021-07-19 12:11:18 +0300

    Update config_fs
    Change-Id: I013691af9ddb8f801b456d92fbfc7a35091dc70b
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- config.fs
+++ config.fs
@@ -19,6 +19,9 @@ value:2906
 [AID_VENDOR_THERMAL]
 value:2907
 
+[AID_VENDOR_FASTRPC]
+value:2908
+
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
@@ -42,6 +45,12 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/imsdaemon]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE WAKE_ALARM BLOCK_SUSPEND
 
 [system/bin/cnss-daemon]
 mode: 0755
